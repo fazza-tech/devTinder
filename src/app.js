72442eb42abde8c43df6b00a25a2c0565b870332
fazza-tech <ebranfazza@gmail.com>
2025-01-14 20:13:43 +0530

    created a server

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req,res) =>{
+    res.send("Welcome")
+})
+
+app.get('/login', (req,res) =>{
+    res.send("Its a Login Page")
+})
+
+app.listen(3000, ()=>{
+    console.log("Server is running on port 3000");
+})
