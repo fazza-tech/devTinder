499491f5cd5bc0bc93817ad93af8ea414960a1cf
fazza-tech <ebranfazza@gmail.com>
2025-01-14 22:56:13 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -2,12 +2,22 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (req,res) =>{
-    res.send("Welcome")
+
+app.get("/user/:userId/:password", (req,res) => {
+    console.log(req.params);
+    res.send({firstName :"Fazza", lastname:"Al Mammazrayillakath"})
 })
 
-app.get('/login', (req,res) =>{
-    res.send("Its a Login Page")
+app.post('/user', (req,res)=>{
+    res.send("Saved data to DB ")
+})
+
+app.delete('/user', (req,res)=>{
+    res.send("Deleted a user")
+})
+
+app.patch('/user', (req,res)=>{
+    res.send("Updated a user  ")
 })
 
 app.listen(3000, ()=>{
