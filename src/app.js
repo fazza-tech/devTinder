bf7f3af16c7662087c50b1d0006e96db8db0ae93
fazza-tech <ebranfazza@gmail.com>
2025-01-15 20:57:21 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -3,20 +3,26 @@ const {adminAuth,userAuth} = require('./middlewares/auth')
 
 const app = express()
 
-app.use('/Admin', adminAuth)
-
-
 
 app.get('/user/login', userAuth, (req,res)=>{
-    res.send('hi user')
+        res.send('hi user')
+        res.status(500).send('Error Happening')
 })
 
 app.get('/Admin/getAllData', (req, res) => {
+    throw new Error('blabla')
     res.send('Here the all data')
 })
 
 app.delete('/Admin/deleteUser', (req, res) => {
+    
     res.send('deleted a user')
+})
+
+app.use('/', (err,req,res,next) => {
+    if(err){
+        res.status(500).send('Error Happening')
+    }
 })
 
 app.listen(3000, () => {
