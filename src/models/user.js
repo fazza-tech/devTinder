d6376b8158192a5882fc10a729d993b11b6e350d
fazza-tech <ebranfazza@gmail.com>
2025-01-16 00:49:15 +0530

    created a schema for user

---
+++ src/models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    firstName : String,
+    lastName : String,
+    emailId : String,
+    password : String,
+    age : String,
+    gender : String
+})
+
+const User = mongoose.model('User', userSchema)
