76b5c45cee4a7e628f35b49d23265d5b6aeef14b
fazza-tech <ebranfazza@gmail.com>
2025-01-18 23:51:22 +0530

    validation added

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose')
 
+const validator = require("validator")
+
+
 const userSchema = new mongoose.Schema({
     firstName :  {
         type: String,
@@ -13,7 +16,12 @@ const userSchema = new mongoose.Schema({
          required:true,
          unique:true,
          lowercase : true,
-         trim : true
+         trim : true,
+         validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("invalid Email address " + value)
+            }
+         }
     },
     password :{ 
         type:String,
@@ -32,7 +40,12 @@ const userSchema = new mongoose.Schema({
         }
     },
     skills : {
-        type:[String]
+        type:[String],
+       validate(value) {
+        if(value.length >=8){
+            throw new Error(" The skills array can have a maximum of 8 skills.")
+        }
+       }
     },
     about : {
         type : String,
@@ -42,6 +55,10 @@ const userSchema = new mongoose.Schema({
         type : String,
         default : "https://www.pngall.com/wp-content/uploads/5/Profile-PNG-File.png"
     }
-})
+},
+    {
+        timestamps:true
+    } 
+)
 
 module.exports = mongoose.model('User', userSchema)
