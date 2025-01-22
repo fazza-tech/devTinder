f3edb643463430e1dd7dd44a85fe5b4dd65c4e9d
fazza-tech <ebranfazza@gmail.com>
2025-01-22 20:55:15 +0530

    imported authRouter

--- src/app.js
+++ src/app.js
@@ -2,70 +2,21 @@ const express = require('express')
 const app = express()
 const connectDB = require('./config/database')
 const User = require('./models/user') //schema
-const { validateSignupData } = require("./utils/validation")
-const bcrypt = require("bcrypt")
-const cookieParser = require("cookie-parser")
-const jwt = require('jsonwebtoken');
-const {userAuth} = require("./middlewares/auth")
 
+const cookieParser = require("cookie-parser")
+const {userAuth} = require("./middlewares/auth")
 app.use(express.json())// its a middlware
 app.use(cookieParser())//middleware for parsing cookie
 
+const authRouter = require("./routes/auth")
+
+app.use("/", authRouter)
+
+
+
 //only work when we call it
-app.post('/signup', async (req, res) => {
 
-    try {
-        const { firstName, lastName, emailId, password } = req.body
-        //validation of data
-        validateSignupData(req)
 
-        // Encrypt of password
-        const hashPassword = await bcrypt.hash(password, 10)
-        console.log(hashPassword);
-        //creating an instance for User schema
-        const user = new User({
-            firstName,
-            lastName,
-            emailId,
-            password: hashPassword
-        })
-        await user.save(); //requesting for save and waiting for promise
-        res.send("data added")
-    } catch (err) {
-        res.status(400).send("Error saving the user:" + err.message)
-    }
-
-})
-
-app.post('/login', async (req, res) => {
-    try {
-        const { emailId, password } = req.body
-
-        //find email is existing or not inDB
-        const user = await User.findOne({ emailId: emailId })
-        if (!user) {
-            throw new Error("Invalid credentials")
-        }
-
-        const isPasswordValid = await user.validatePassword(password)
-        if (isPasswordValid) {
-
-            //create a JWT Token 
-            const token = await user.getJWT()
-            
-
-            // Add the token to cookie and send the response back to the user
-            res.cookie('token',token, {
-                expires: new Date(Date.now() + 8 * 3600000)})
-            res.send("login succesfull!!!")
-        }else{
-            throw new Error("Invalid credentials")
-        }
-    }catch(err){
-        res.status(400).send("Error saving the user:" + err.message)
-    }
-   
-})
 
 app.get('/profile',userAuth, async (req,res)=>{
     try{
