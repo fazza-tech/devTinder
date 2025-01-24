0c056c74b1d29a649a1aba95d1e4df1c13ececa1
fazza-tech <ebranfazza@gmail.com>
2025-01-24 21:37:34 +0530

    profile view and profile edit API

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,16 +1,34 @@
 const express = require("express")
-const {userAuth} = require("../middlewares/auth")
+const { userAuth } = require("../middlewares/auth")
 const profileRouter = express.Router()
+const { validateProfileEdit } = require("../utils/validation")
 
-profileRouter.get('/profile',userAuth, async (req,res)=>{
-    try{
-    const user = req.user;
-    
-    res.send(user)
-    }catch(err){
+profileRouter.get('/profile/view', userAuth, async (req, res) => {
+    try {
+        const user = req.user;
+
+        res.send(user)
+    } catch (err) {
         res.status(400).send("Error saving the user:" + err.message)
     }
-    
+
+})
+
+profileRouter.patch('/profile/edit', userAuth, async (req, res) => {
+    try {
+
+        if (!validateProfileEdit(req)) {
+            throw new Error("Invalid edit request")
+        }
+
+        const loggedUser = req.user
+        console.log(loggedUser);
+        Object.keys(req.body).every(key => loggedUser[key] = req.body[key])
+        console.log();
+    } 
+    catch (err) {
+        res.status(400).send("ERROR: " + err.message)
+    }
 })
 
 module.exports = profileRouter
