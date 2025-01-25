67894bc251b23344eedfde847ecf8e355174e2f9
fazza-tech <ebranfazza@gmail.com>
2025-01-25 23:21:20 +0530

    added enum validation into status

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -1,16 +1,23 @@
 const mongoose = require('mongoose')
 
-const connectionRequest = new mongoose.Schema({
+const connectionRequest = new mongoose.Schema(
+    {
 
-    fromUserId: {
-        type: mongoose.Schema.Types.ObjectId
+        fromUserId: {
+            type: mongoose.Schema.Types.ObjectId
+        },
+        toUserId: {
+            type: mongoose.Schema.Types.ObjectId
+        },
+        status: {
+            type: String,
+            enum: {
+                values: ["ignore", "interested", "accepted", "rejected"],
+                message: `{VALUE} is incorrected status type`
+            }
+
+        }
     },
-    toUserId: {
-        type: mongoose.Schema.Types.ObjectId
-    },
-    status: {
-        type: String,
-        
-            
-    }
-})
+    {
+        timestamps: true
+    })
